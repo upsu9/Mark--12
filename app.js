16b9cbe6542058e1b92d8207f724f3262028e30d
upsu9 <upsu10@gmail.com>
2021-09-24 22:50:44 +0530

    till Is Triangle withou css

---
+++ app.js
@@ -0,0 +1,31 @@
+var input = document.querySelectorAll(".input");
+var inputBtn = document.querySelector("#inputBtn");
+var clearBtn = document.querySelector("#clearBtn");
+var errorBox = document.querySelector("#errorBox");
+var outputBox = document.querySelector("#outputBox");
+
+inputBtn.addEventListener("click", inputClick)
+/* clearBtn.addEventListener("click", clear) */
+
+
+
+function inputClick() {
+    var angleOne = Number(input[0].value);
+    var angleTwo = Number(input[1].value);
+    var angleThree = Number(input[2].value);
+
+    calculate(angleOne, angleTwo, angleThree)
+
+}
+
+function calculate(anOne, anTwo, anThree) {
+
+    if (anOne > 0 && anTwo > 0 && anThree > 0) {
+        var result = anThree + anTwo + anOne;
+        if (result === 180) {
+            outputBox.innerText = "Hurrah! Entered Angles make Triangle"
+        } else {
+            outputBox.innerText = "Sadly ! Entered Angles doesn't make any Triangle."
+        }
+    } else errorBox.innerText = "All angles should be greater than zero"
+}
