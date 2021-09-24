2e1cbfb00a4821b041f34740d2880f7563e1d574
upsu9 <upsu10@gmail.com>
2021-09-25 02:09:59 +0530

    hypo without css

---
+++ hypo.js
@@ -0,0 +1,34 @@
+var inpuBtn = document.querySelector("#inputBtn")
+var input = document.querySelectorAll(".input")
+var outputBox = document.querySelector("#outputBox")
+
+inpuBtn.addEventListener("click", clickHandler)
+
+/* const inputA = Number(input[0].value);
+
+const inputB = Number(input[1].value); */
+
+function clickHandler()
+{
+    var inputA = Number(input[0].value);
+
+    var inputB = Number(input[1].value);
+    console.log (inputA)
+    
+finalResult(inputA,inputB)
+
+}
+
+function finalResult (inputOne, inputTwo)
+{
+   
+    var a = inputOne * inputOne;
+    var b = inputTwo * inputTwo;
+    console.log (a,b)
+const result = (Math.sqrt(a+b));
+const resultFinal = result.toFixed(2)
+
+outputBox.innerText = `The hypotenuse of triangle is ${resultFinal}.`
+
+
+}
