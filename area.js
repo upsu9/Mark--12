6bdafc5b434ae5746269133193d7129b3f7eed4d
upsu9 <upsu10@gmail.com>
2021-09-25 02:28:12 +0530

    final triangle without css

---
+++ area.js
@@ -0,0 +1,31 @@
+var inpuBtn = document.querySelector("#inputBtn")
+var input = document.querySelectorAll(".input")
+var outputBox = document.querySelector("#outputBox")
+
+inpuBtn.addEventListener("click", clickHandler)
+
+/* const inputA = Number(input[0].value);
+
+const inputB = Number(input[1].value); */
+
+function clickHandler()
+{
+    var inputA = Number(input[0].value);
+
+    var inputB = Number(input[1].value);
+    console.log (inputA)
+    
+finalResult(inputA,inputB)
+
+}
+
+function finalResult (inputOne, inputTwo)
+{
+   
+    var a = inputOne * inputTwo;
+  
+const result = ( 0.5 * a);
+const resultFinal = result.toFixed(2)
+
+outputBox.innerText = `The Area of triangle is ${resultFinal}.`
+}
